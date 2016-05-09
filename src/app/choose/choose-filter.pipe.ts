f8769dff1a836350c5f97c6b770f1b67a49ac34d
Humakt83 <urlact@yahoo.com>
2016-05-09 12:25:13 +0300

    Option to query by scientific name

--- src/app/choose/choose-filter.pipe.ts
+++ src/app/choose/choose-filter.pipe.ts
@@ -6,8 +6,12 @@ import { Plant } from '../shared/index';
 })
 export class ChooseFilterPipe {
     
-    transform(plants: Plant[], query: string) {
-        if (!query) return plants;
-        return plants.filter(plant => plant.name.toLowerCase().includes(query.toLowerCase()));
+transform(living: any[], query: string, scientific: boolean) {
+        if (!query) return living;
+        if (scientific) {
+            return living.filter(living => living.scientificName.toLowerCase().includes(query.toLowerCase()));
+        } else {
+            return living.filter(living => living.name.toLowerCase().includes(query.toLowerCase()));
+        }
     }
 }
