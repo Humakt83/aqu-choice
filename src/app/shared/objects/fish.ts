d0cb2d179380158367a8c169254af5f9e06c12c8
Humakt83 <urlact@yahoo.com>
2016-12-08 21:58:15 +0200

    Add size attribute to fish

--- src/app/shared/objects/fish.ts
+++ src/app/shared/objects/fish.ts
@@ -3,9 +3,9 @@ import { Living } from './living';
 
 export class Fish implements Living {
   
-    constructor(public scientificName: string, public name: string, public optimalWater: OptimalWater, public minimumTankSize: number) {}
+    constructor(public scientificName: string, public name: string, public optimalWater: OptimalWater, public minimumTankSize: number, public size: number) {}
     
     toString() : string {
-        return `${this.scientificName}: (${this.optimalWater.toString()}, akvaarion koko: ${this.minimumTankSize} l)`
+        return `${this.scientificName}: (pituus: ${this.size} cm, ${this.optimalWater.toString()}, akvaarion koko: ${this.minimumTankSize} l)`
     }
 }
