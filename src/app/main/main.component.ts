28862d134015b10268ec8ffb3f79f9a8b1f9a0bd
Humakt83 <urlact@yahoo.com>
2016-12-20 21:45:12 +0200

    Set language as query param

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -11,11 +11,34 @@ export class MainComponent {
 
     constructor(private translate: TranslateService) {
         translate.setDefaultLang('fi');
-        translate.use(this.selectedLanguage);
+        this.setLanguageFromQueryParam();
     }
 
     toggleLanguage() {
-        this.selectedLanguage = this.selectedLanguage === 'fi'? 'en' : 'fi';
+        this.selectedLanguage === 'fi'? this.changeLanguage('en') : this.changeLanguage('fi');
         this.translate.use(this.selectedLanguage);
     }
+
+    private changeLanguage(lang: string) {
+        this.selectedLanguage = lang;
+        this.setLanguageParam(lang);
+    }
+
+    private setLanguageFromQueryParam() {
+        let url = window.location.href;
+        if (url.includes('lang=')) {
+            this.selectedLanguage = url.split('lang=')[1];
+        } else {
+            this.setLanguageParam(this.selectedLanguage);
+        }
+        this.translate.use(this.selectedLanguage);
+    }
+
+    private setLanguageParam(lang: string) {
+        if (history.pushState) {
+            let newurl = window.location.protocol + "//" + window.location.host + window.location.pathname + '?lang=' + lang;
+            window.history.pushState({path:newurl},'',newurl);
+        }  
+    }
+
 }
